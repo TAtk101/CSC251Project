eb3204bb3d04afed2bc9c9e65880a4fbbc2e289e
TAtk101 <tayloratkinsonn@yahoo.com>
2024-06-26 13:24:29 -0400

    Update PolicyHolder.java
    Imported fields and methods from Policy class

---
+++ PolicyHolder.java
@@ -0,0 +1,73 @@
+public class PolicyHolder {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String smokingStatus;
+    private int height;  // height in inches
+    private int weight;  // weight in pds
+
+    // Constructors
+    public PolicyHolder() {
+        this("", "", 0, "", 0, 0);
+    }
+
+    public PolicyHolder(String firstName, String lastName, int age, String smokingStatus, int height, int weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.smokingStatus = smokingStatus;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // Getter and Setter 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSmokingStatus() {
+        return smokingStatus;
+    }
+
+    public void setSmokingStatus(String smokingStatus) {
+        this.smokingStatus = smokingStatus;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+   
+}
